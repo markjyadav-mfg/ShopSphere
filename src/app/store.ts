1c4634fda953e4b0cc7095c9ce24bed3d2f46819
mark j yadav <yadavjitendre359@gmail.com>
2026-04-16 18:05:56 +0530

    Fix AppDispatch type export in store.ts

--- src/app/store.ts
+++ src/app/store.ts
@@ -15,3 +15,23 @@ export const store = configureStore({
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
+
+
+// import { configureStore } from '@reduxjs/toolkit';
+// import cartReducer from '../features/cart/cartSlice';
+// import wishlistReducer from '../features/wishlist/wishlistSlice';
+// import toastReducer from '../features/toast/toastSlice';
+// import commentReducer from '../features/comment/commentSlice';
+
+// export const store = configureStore({
+//   reducer: {
+//     cart: cartReducer,
+//     wishlist: wishlistReducer,
+//     toast: toastReducer,
+//     comment: commentReducer,
+//   },
+// });
+
+// export type RootState = ReturnType<typeof store.getState>;
+// export type AppDispatch = typeof store.dispatch;
